7fd6b0661cd5c709060c2268159cd7dcbc569253
Pilar Montenegro <nadiapilarm@gmail.com>
2023-06-08 21:26:50 -0300

    vistas y urls

---
+++ proyectoRieVictoria/appRieVictoria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [path ('',views.index, name='index'),
+               path ('contador/',views.contador, name='contador'),
+               path ('bienvenida/',views.bienvenida, name='bienvenida')
+               
+               ]
--- proyectoRieVictoria/appRieVictoria/views.py
+++ proyectoRieVictoria/appRieVictoria/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Articulos, Empleados, Proveedores
+
+def index(request):
+    mensaje = f"<html><h2> Bienvenidos al sistema Rie Victoria</h2>"\
+    f"<p> Este es un sistema de control de stock</p></html>"
+    return HttpResponse(mensaje)
+
+def contador(request):
+    mensaje = f"<html><h2> Bienvenidos al sistema Rie Victoria</h2>"\
+    f"<p> Este es un sistema de control de stock</p></html>"\
+    f"<p>En el sistema hay {Articulos.objects.count()}articulos cargados</p></html>"
+    return HttpResponse(mensaje)
+
+def bienvenida(request):
+    #un nombre con el que llamo a la variable : modelo objeto que voy a mandar                                 
+    context = {'Articulos': Articulos.objects.all()}
+    #template, el render manda el contexto al template||
+    return render(request, 'bienvenida.html', context)
