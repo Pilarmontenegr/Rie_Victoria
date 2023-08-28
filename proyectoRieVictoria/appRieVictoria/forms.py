65d81e99dd63d8c3e9c8501433823fc53dd662b4
Pilar Montenegro <nadiapilarm@gmail.com>
2023-08-28 18:12:38 -0300

    cambios pili

--- proyectoRieVictoria/appRieVictoria/forms.py
+++ proyectoRieVictoria/appRieVictoria/forms.py
@@ -1,14 +1,4 @@
 from django import forms
 from .models import Articulos
 
-class ArticulosForm(forms.ModelForm):
-    class Meta:
-        model = Articulos
-        fields = ('descripcion', 'costo', 'venta', 'cantidad', 'talle', 'tipoPrenda')
-        widgets = {
-            'descripcion': forms.TextIput(attrs={'class': 'form-control'}),
-            'costo': forms.NumberInput(attrs={'class': 'form-control'}),
-            'venta': forms.NumberInput(attrs={'class': 'form-control'}),
-            'cantidad': forms.NumberInput(attrs={'class': 'form-control'}),
-            'descripcion': forms.TextIput(attrs={'class': 'form-control'}),
-        }
+#aca hago lo de proveedores
