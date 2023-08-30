8b242eaf444b889ad0607e803d68a992d2fb4a6a
Pilar Montenegro <nadiapilarm@gmail.com>
2023-08-30 20:29:08 -0300

    cambios

--- proyectoRieVictoria/appRieVictoria/urls.py
+++ proyectoRieVictoria/appRieVictoria/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [path ('',views.index, name='index'),
                path('proveedores/ProveedoresModif/<str:pk>/', views.ProveedoresModif, name='ProveedoresModif'),
                path('proveedores/ProveedoresBorrar/<str:pk>/', views.ProveedoresBorrar, name='ProveedoresBorrar'),
                ]
-=======
-               ] 
+ 
 
 >>>>>>> 8351b7fc631f7d728f7c05ea79902569f91eb6b1
